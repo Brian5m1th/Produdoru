395102600498286a177eeff4c5503a226fd9fdd4
Brian Smith <briandiscbol@gmail.com>
2025-04-16 15:46:56 -0300

    fix:consertar o api e domain

---
+++ src/main/java/dev/wakanda/produdoro/usuario/application/api/UsuarioAPI.java
@@ -0,0 +1,9 @@
+package dev.wakanda.produdoro.usuario.application.api;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping(value = "/api/usuario")
+public class UsuarioAPI {
+}
---
+++ src/main/java/dev/wakanda/produdoro/usuario/application/api/UsuarioController.java
@@ -0,0 +1,16 @@
+package dev.wakanda.produdoro.usuario.application.api;
+
+import lombok.Getter;
+import lombok.extern.log4j.Log4j2;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@Log4j2
+@Getter
+public class UsuarioController extends UsuarioAPI {
+
+    public UsuarioCriadoResponse PostUsuario(UsuarioNovoRequest UsuarioNovo) {
+        log.info("[start] UsuarioController - PostUsuario");
+        return null;
+    }
+}
